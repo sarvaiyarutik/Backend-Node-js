42b9e059f01f22711b4d4cf10a37e68777d18fbd
Sarvaiya Rutik <sarvaiyarutik18@gmail.com>
2026-08-24 11:43:06 +0530

    added code

--- 06_express_crud/Express_3/server.js
+++ 06_express_crud/Express_3/server.js
@@ -1,1 +1,83 @@
 
+
+import express from "express";
+import httpError from "./middleware/httpError.js"
+const app = express();
+
+
+const tasksList = [
+    {
+        id: 1,
+        task: "Learn JavaScript",
+        description: "Practice arrays, objects, functions, and DOM."
+    },
+    {
+        id: 2,
+        task: "Learn Node.js",
+        description: "Understand modules, file system, and servers."
+    },
+    {
+        id: 3,
+        task: "Learn Express.js",
+        description: "Practice routes, middleware, and CRUD operations."
+    },
+    {
+        id: 4,
+        task: "Build a Project",
+        description: "Create a simple Task Management application."
+    }
+];
+
+app.get("/",(req,res)=>{
+
+    return res.json({message:"Express crud"});
+
+})
+
+app.get("/taskList",(req,res)=>{
+
+    if(tasksList.length === 0){
+
+        return res.status(200).json({message:"no task available"});
+    }
+
+    res.status(200).json({message:"task added successfully",tasksList});
+
+})
+
+
+// Undefine Middleware
+
+app.use((req,res,next)=>{
+
+    return next(new httpError("Request not found"))
+
+})
+
+app.use((error,req,res,next)=>{
+
+    if(res.headersSent){
+
+        return next(error)
+
+    }
+
+
+    res.status(error.statusCode || 500).json({message:error.message || "Internal Server error "})
+
+})
+
+
+const port = 1000;
+
+app.listen(port,(err)=>{
+
+    if(err){
+
+        return console.log(err)
+    }
+
+    console.log(`server running on port ${port}`)
+
+
+})
