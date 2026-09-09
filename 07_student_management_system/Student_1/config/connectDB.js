e7f28ae44d7cb051f3948cf23e6c19094384d839
Sarvaiya Rutik <sarvaiyarutik18@gmail.com>
2026-09-09 11:40:04 +0530

    added code

---
+++ 07_student_management_system/Student_1/config/connectDB.js
@@ -0,0 +1,28 @@
+
+
+import mongoose from "mongoose";
+
+
+async function connectDB(){
+
+    try{
+
+        const connect = await mongoose.connect("mongodb://127.0.0.1:27017/studentManagementSystem")
+
+    console.log("DB Connected");
+
+
+
+    return connect;
+
+    }catch(err){
+
+        console.log(error.massage);
+
+    }
+
+
+}
+
+
+export default connectDB;
